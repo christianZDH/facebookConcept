395d07ea814b54f6a90607ebb28aa9df72e086d1
christianZDH <cristian5732z@outlook.com>
2022-03-31 06:10:03 -0800

    tab bar

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,8 +3,8 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'facebookredesign',
-  webDir: 'www',
-  bundledWebRuntime: false
+  webDir: 'dist/facebookredesign',
+  bundledWebRuntime: false,
 };
 
 export default config;
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,12 +1,49 @@
 import { Component } from '@angular/core';
 
+interface TabsI {
+  tab: string;
+  icon: string;
+  notification: boolean;
+}
 @Component({
   selector: 'app-tabs',
   templateUrl: 'tabs.page.html',
-  styleUrls: ['tabs.page.scss']
+  styleUrls: ['tabs.page.scss'],
 })
 export class TabsPage {
+  tabs: TabsI[] = [];
+  tabSelected = '';
+  constructor() {
+    this.tabs = [
+      {
+        tab: 'tab1',
+        icon: 'assets/icon/home-facebook.svg',
+        notification: false,
+      },
+      {
+        tab: 'tab2',
+        icon: 'assets/icon/watch-facebook.svg',
+        notification: true,
+      },
+      {
+        tab: 'tab3',
+        icon: 'assets/icon/market-facebook.svg',
+        notification: true,
+      },
+      {
+        tab: 'tab4',
+        icon: 'assets/icon/people-facebook.svg',
+        notification: false,
+      },
+      {
+        tab: 'tab5',
+        icon: 'assets/icon/menu-facebook.svg',
+        notification: false,
+      },
+    ];
+  }
 
-  constructor() {}
-
+  onTabsChange(ev: any) {
+    this.tabSelected = ev.tab;
+  }
 }
