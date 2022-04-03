eeca0c1b89683ebbade265c6f12f971f697a519a
christianZDH <cristian5732z@outlook.com>
2022-04-02 23:39:54 -0800

    get user from service

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../services/user.service';
+import { UserI } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-toolbar',
@@ -6,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./toolbar.component.scss'],
 })
 export class ToolbarComponent implements OnInit {
+  user: UserI;
+  constructor(private userService: UserService) {}
 
-  constructor() { }
-
-  ngOnInit() {}
-
+  ngOnInit() {
+    this.userService.getUser().subscribe((user: UserI) => {
+      this.user = user;
+    });
+  }
 }
