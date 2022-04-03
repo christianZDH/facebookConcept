163bb613fe071ef06b5859359bb9f17a3786279a
christianZDH <cristian5732z@outlook.com>
2022-04-02 23:33:34 -0800

    user image service

--- src/app/components/stories/stories.component.ts
+++ src/app/components/stories/stories.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SwiperOptions } from 'swiper';
 import { StorieI } from '../../interfaces/storie.interface';
+import { UserI } from '../../interfaces/user.interface';
 
 @Component({
   selector: 'app-stories',
@@ -9,6 +10,7 @@ import { StorieI } from '../../interfaces/storie.interface';
 })
 export class StoriesComponent implements OnInit {
   @Input() stories: StorieI[] = [];
+  @Input() user: UserI;
   config: SwiperOptions = {
     slidesPerView: 4.5,
     lazy: { loadPrevNext: true },
