852dada6d00f1dc93ebbbae5c8cacafd38fa5dde
christianZDH <cristian5732z@outlook.com>
2022-04-06 20:19:35 -0800

    redirect to profile

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../../services/user.service';
 import { UserI } from '../../interfaces/user.interface';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-toolbar',
@@ -9,11 +10,15 @@ import { UserI } from '../../interfaces/user.interface';
 })
 export class ToolbarComponent implements OnInit {
   user: UserI;
-  constructor(private userService: UserService) {}
+  constructor(private userService: UserService, private route: Router) {}
 
   ngOnInit() {
     this.userService.getUser().subscribe((user: UserI) => {
       this.user = user;
     });
+  }
+
+  redirectToProfile() {
+    this.route.navigate(['/profile']);
   }
 }
